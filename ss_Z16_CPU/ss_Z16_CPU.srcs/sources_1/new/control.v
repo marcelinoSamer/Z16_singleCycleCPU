8ca08220b006d66dced206b7675585cc1c4d0d4c
Marcelino10 <marcelino.sedhum@aucegypt.edu>
2025-05-12 22:08:26 +0200

    initial heirarchy

---
+++ ss_Z16_CPU/ss_Z16_CPU.srcs/sources_1/new/control.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/12/2025 11:07:58 PM
+// Design Name: 
+// Module Name: control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control(
+
+    );
+endmodule
